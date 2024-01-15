e94dcc0707a3f18c9fd28e9b87fc79cc339f15cc
nixowl <gabriellamondvay@gmail.com>
2024-01-15 21:04:52 +0100

    add tailwind and shadcn

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './index.css'
+import { Button } from "@/components/ui/button"
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header className="px-4 py-2 flex items-center justify-center">
+        Header
+      </header>
+
+      <main className="flex-1 bg-red-500 flex items-center justify-center">
+        Content and a button:<br />
+        <Button>Test button</Button>
+    </main>
+
+    <footer className="px-4 py-2 flex items-center justify-center">
+      Footer
+     </footer>
     </>
   )
 }
