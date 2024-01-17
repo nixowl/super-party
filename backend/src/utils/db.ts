d7d8ed0af119c55a4c15c770c9bd2b703cb8b1f9
Luka <lukabrkovic@skiff.com>
2024-01-18 00:16:29 +0100

    CHOR( table studio) db:studio script

--- backend/src/utils/db.ts
+++ backend/src/utils/db.ts
@@ -1,5 +1,6 @@
 import postgres from "postgres"
 import { drizzle } from "drizzle-orm/postgres-js"
+import "dotenv/config"
 
 const queryClient = postgres(process.env.DATABASE_URL!)
 export const db = drizzle(queryClient)
